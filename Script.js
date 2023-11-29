7a8db62329e70e5211a52e3b4383719877788943
Fortunebeast272822 <143697886+Fortunebeast272822@users.noreply.github.com>
2023-11-29 01:04:42 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,37 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const stars = document.querySelectorAll('.star-rating');
 
+    stars.forEach(star => {
+        star.addEventListener('click', function (e) {
+            const rating = e.target.dataset.rating || e.target.parentNode.dataset.rating;
+            resetStars(star);
+            star.dataset.rating = rating;
+        });
+
+        star.addEventListener('mouseover', function (e) {
+            const hoverRating = e.target.dataset.rating || e.target.parentNode.dataset.rating;
+            resetStars(star);
+            setHoverStars(star, hoverRating);
+        });
+
+        star.addEventListener('mouseout', function () {
+            const rating = star.dataset.rating;
+            resetStars(star);
+            setHoverStars(star, rating);
+        });
+    });
+
+    function resetStars(starContainer) {
+        starContainer.querySelectorAll('.star-rating').forEach(star => {
+            star.style.color = '#ddd';
+        });
+    }
+
+    function setHoverStars(starContainer, hoverRating) {
+        starContainer.querySelectorAll('.star-rating').forEach(star => {
+            if (star.dataset.rating <= hoverRating) {
+                star.style.color = '#ffcc29';
+            }
+        });
+    }
+});
